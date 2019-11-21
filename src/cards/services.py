89407cddaf6a75d407f08f6e03d951a71080b364
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2019-11-22 00:53:20 +0100

    PEP8

--- src/cards/services.py
+++ src/cards/services.py
@@ -5,13 +5,13 @@ from cards.card import Card
 from api.services import get_cards_as_rectangles
 from data_structures.rectangles import Rectangle
 from data_structures.services import get_id_of_rectangle_hovered_by_mouse, get_id_of_rectangle_in_y_coord
-from input.utils import Field, get_fields_y_coords
 
 
-def  get_card_from_rectangles_in_a_position(position: (int, int), rectangles: [Rectangle], cards: {str: Card}) -> Card:
+def get_card_from_rectangles_in_a_position(position: (int, int), rectangles: [Rectangle], cards: {str: Card}) -> Card:
     card_id = get_id_of_rectangle_hovered_by_mouse(position, rectangles)
     print(card_id)
     return cards.get(card_id, None)
+
 
 def get_card_from_rectangles_by_y_coord(y_coord: int, rectangles: [Rectangle], cards: {str: Card}):
     cards_ids = get_id_of_rectangle_in_y_coord(y_coord, rectangles)
@@ -19,9 +19,11 @@ def get_card_from_rectangles_by_y_coord(y_coord: int, rectangles: [Rectangle], c
         cards.get(card_id, None) for card_id in cards_ids
     )
 
+
 async def get_card_in_position(position: (int, int), cards: {str: Card}) -> Card:
     rectangles: [Rectangle] = await get_cards_as_rectangles()
     return get_card_from_rectangles_in_a_position(position, rectangles, cards)
+
 
 async def get_cards_in_field_by_y_coord(field_y_coord, cards: {str: Card}):
     rectangles: [Rectangle] = await get_cards_as_rectangles()
