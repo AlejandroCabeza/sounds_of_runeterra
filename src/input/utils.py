df9426b94d98fd65356b321e70db3e49a52ea2f2
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2019-11-22 00:52:38 +0100

    Added missing values to Field enum and to get_fields_y_coords.

--- src/input/utils.py
+++ src/input/utils.py
@@ -9,18 +9,24 @@ def transform_mouse_position_to_bottom_left_coordinate_axis(top_left_mouse_posit
     _, screen_height = SCREEN_SIZE
     return top_left_mouse_position[0], screen_height - top_left_mouse_position[1]
 
+
 class Field(enum.Enum):
-    PLAYER_HAND : int = 0
-    PLAYER_PLAYED : int = 1
-    OPPONENT_HAND : int = 2
-    OPPONENT_PLAYED : int = 3
+    PLAYER_HAND: int = 0
+    PLAYER_PLAYED: int = 1
+    PLAYER_BATTLEFIELD: int = 2
+    OPPONENT_HAND: int = 3
+    OPPONENT_PLAYED: int = 4
+    OPPONENT_BATTLEFIELD: int = 5
+
 
 def get_fields_y_coords(vertical_screen_size):
     field_size = vertical_screen_size / 4
     chunk_size = vertical_screen_size / 8
     return {
-        Field.PLAYER_HAND : chunk_size,
-        Field.PLAYER_PLAYED : chunk_size + field_size,
-        Field.OPPONENT_PLAYED: vertical_screen_size - chunk_size - field_size,
-        Field.OPPONENT_HAND : vertical_screen_size - chunk_size,
+        Field.PLAYER_HAND: 1,
+        Field.PLAYER_PLAYED: chunk_size,
+        Field.PLAYER_BATTLEFIELD: chunk_size + field_size,
+        Field.OPPONENT_HAND: vertical_screen_size - 1,
+        Field.OPPONENT_PLAYED: vertical_screen_size - chunk_size,
+        Field.OPPONENT_BATTLEFIELD: vertical_screen_size - chunk_size - field_size,
     }
